b96f198f5f23bbd279cc3ab3f22c556a70103dfd
wanda <wanda@LAPTOP-1MESO504>
2022-12-21 23:02:08 -0300

    Agregar archivos de proyecto.

---
+++ Cupcakes/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Cupcakes.Data;
+using Cupcakes.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cupcakes
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ICupcakeRepository, CupcakeRepository>();
+
+            services.AddDbContext<CupcakeContext>(options =>
+            options.UseSqlServer(_configuration.GetConnectionString("BakeriesDbConnection")));
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            //cupcakeContext.Database.EnsureDeleted();
+            //cupcakeContext.Database.EnsureCreated();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "CupcakeRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Cupcake", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
